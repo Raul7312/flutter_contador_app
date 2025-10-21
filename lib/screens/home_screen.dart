0d8c9cbaa83ac7e74759b4d65acfc9b3cc0113c2
raul <raulperezgutierrez77@gmail.com>
2025-10-21 11:06:03 +0200

    clase con las funciones

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,12 +10,27 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int contador = 0;
 
+  void incrementar() {
+    contador++;
+    setState(() {});
+  }
+
+  void decrementar() {
+    contador--;
+    setState(() {});
+  }
+
+  void reset() {
+    contador = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     const tamano30 = TextStyle(fontSize: 30);
 
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
+      backgroundColor: Colors.lightBlueAccent,
       appBar: AppBar(title: Text('Contador'), elevation: 10.0),
       body: Center(
         child: Column(
@@ -27,20 +42,49 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Row(
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              print('Se ha registrado una pulsación');
-              contador++;
-              setState(() {});
-              print('$contador');
-            },
-          ),
-        ],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: CustomFloatingActionButton(
+        incrementarFn: incrementar,
+        decrementarFn: decrementar,
+        resetFn: reset,
       ),
+    );
+  }
+}
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final Function incrementarFn;
+  final Function decrementarFn;
+  final Function resetFn;
+
+  const CustomFloatingActionButton({
+    super.key,
+    required this.incrementarFn,
+    required this.decrementarFn,
+    required this.resetFn,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.remove),
+          //child: const Text('-1'),
+          onPressed: () => decrementarFn(),
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_zero_rounded),
+          //child: const Text('RESET'),
+          onPressed: () => resetFn(),
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.add),
+          //child: const Text('+1'),
+          onPressed: () => incrementarFn(),
+        ),
+      ],
     );
   }
 }
